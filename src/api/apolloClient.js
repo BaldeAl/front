3c7ec2f2d183f30576cf975b6a474e1d41d89e11
BALDE <79581163+BaldeAl@users.noreply.github.com>
2023-08-17 16:04:15 +0200

    modified:   src/api/apolloClient.js

--- src/api/apolloClient.js
+++ src/api/apolloClient.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 // sont les dépendances nécessaires pour utiliser Apollo Client dans notre application React.
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3000/dev/graphql', // ceci est l'URL de votre API GraphQL
+  uri: 'https://main.d2qway82dzlpqd.amplifyapp.com', // ceci est l'URL de votre API GraphQL
 });
 
 const client = new ApolloClient({
